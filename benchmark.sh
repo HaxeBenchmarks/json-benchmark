d0df1daa1e05af20e19fa2e51a9b553f98cae1d5
AlexHaxe <Alexander.Blum@gmail.com>
2020-04-13 21:08:16 +0200

    run some targets twice to smooth out their graphs

--- benchmark.sh
+++ benchmark.sh
@@ -18,3 +18,18 @@ php out/php/index.php $DATA
 neko out/JsonBenchmark.n $DATA
 python3 out/JsonBenchmark.py $DATA
 # lua out/JsonBenchmark.lua $DATA
+
+# second run to smooth graphs a little
+./out/cpp/JsonBenchmark $DATA
+./out/cppGCGen/JsonBenchmark $DATA
+node out/JsonBenchmark.js $DATA
+node out/JsonBenchmark.es6.js $DATA
+java -jar out/java/JsonBenchmark.jar $DATA
+java -jar out/jvm/JsonBenchmark.jar $DATA
+hl out/JsonBenchmark.hl $DATA
+./out/hl/JsonBenchmark $DATA
+mono out/cs/bin/JsonBenchmark.exe $DATA
+haxe build/common.hxml --run JsonBenchmark $DATA
+php out/php/index.php $DATA
+neko out/JsonBenchmark.n $DATA
+python3 out/JsonBenchmark.py $DATA
